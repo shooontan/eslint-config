39ecdb39c123bb6e544303390ac43e9214b14964
shooontan <logo.bee.doo19@gmail.com>
2021-10-19 06:03:16 +0900

    add moduleNameMapper option for typescript-eslint in jest

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   transform: {
     '\\.[jt]sx?$': '@swc/jest',
   },
+  moduleNameMapper: {
+    // HACK: let typescript-eslint load eslint's exports file.
+    // https://github.com/facebook/jest/issues/9771
+    'eslint/use-at-your-own-risk': 'eslint/lib/unsupported-api.js',
+  },
 };
